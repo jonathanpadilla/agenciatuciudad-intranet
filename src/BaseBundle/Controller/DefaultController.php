06b7e042842accbedb4f49cf8d5f3c08f4d17777
Jonathan Padilla <jonathanpadilla09@outlook.com>
2016-11-18 09:21:17 -0300

    config correo

--- src/BaseBundle/Controller/DefaultController.php
+++ src/BaseBundle/Controller/DefaultController.php
@@ -8,6 +8,134 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('BaseBundle:Default:index.html.twig');
+    	// test json
+    	$arr = [
+    		'1' => [
+    			'nombre' 	=> 'Letrero',
+    			'imagen' 	=> 'placeholder.png',
+    			'visible'	=> true,
+    			'valor'		=> 0,
+    			'next' 		=> 'seleccionarItem(1)',
+    			'sub'		=> [
+    				'1' => [
+    					'nombre' 	=> 'Backlight',
+    					'imagen' 	=> 'placeholder.png',
+    					'visible'	=> true,
+    					'valor'		=> 0,
+    					'next' 		=> 'seleccionarItem(1)',
+    					'sub'		=> [
+    						'4' => [
+    							'nombre' 	=> 'Con caja',
+		    					'imagen' 	=> 'placeholder.png',
+		    					'visible'	=> true,
+    							'valor'		=> 0,
+		    					'next' 		=> 'seleccionarItem(4)',
+		    					'sub'		=> [
+		    						'6' => [
+		    							'nombre' 	=> 'Con luz',
+				    					'imagen' 	=> 'placeholder.png',
+				    					'visible'	=> true,
+    									'valor'		=> 0,
+				    					'next' 		=> 'seleccionarItem(6)',
+				    					'sub' 		=> false,
+		    						],
+		    						'7' => [
+		    							'nombre' 	=> 'Sin luz',
+				    					'imagen' 	=> 'placeholder.png',
+				    					'visible'	=> false,
+    									'valor'		=> 0,
+				    					'next' 		=> 'seleccionarItem(7)',
+				    					'sub' 		=> false,
+		    						],
+		    					]
+    						],
+    						'5' => [
+    							'nombre' 	=> 'Sin caja',
+		    					'imagen' 	=> 'placeholder.png',
+		    					'visible'	=> false,
+    							'valor'		=> 0,
+		    					'next' 		=> 'seleccionarItem(5)',
+		    					'sub' 		=> false,
+    						],
+    					]
+    				],
+    				'2' => [
+    					'nombre' 	=> 'Pendon',
+    					'imagen' 	=> 'placeholder.png',
+    					'visible'	=> true,
+    					'valor'		=> 0,
+    					'next' 		=> 'seleccionarItem(2)',
+    					'sub' 		=> false,
+    				],
+    				'3' => [
+    					'nombre' 	=> 'Acrilico',
+    					'imagen' 	=> 'placeholder.png',
+    					'visible'	=> true,
+    					'valor'		=> 0,
+    					'next' 		=> 'seleccionarItem(3)',
+    					'sub' 		=> false,
+    				],
+    			]
+    		],
+    		'2' => [
+    			'nombre' 	=> 'Páginas web',
+    			'imagen' 	=> 'placeholder.png',
+    			'visible'	=> true,
+    			'valor'		=> 0,
+    			'next' 		=> 'seleccionarItem(2)',
+    			'sub' 		=> false,
+    		],
+    		'3' => [
+    			'nombre' 	=> 'Papeleria',
+    			'imagen' 	=> 'placeholder.png',
+    			'visible'	=> true,
+    			'valor'		=> 0,
+    			'next' 		=> 'seleccionarItem(3)',
+    			'sub' 		=> false,
+    		],
+    		'4' => [
+    			'nombre' 	=> 'Intranet',
+    			'imagen' 	=> 'placeholder.png',
+    			'visible'	=> true,
+    			'valor'		=> 0,
+    			'next' 		=> 'seleccionarItem(4)',
+    			'sub' 		=> false,
+    		],
+    		'5' => [
+    			'nombre' 	=> 'Branding',
+    			'imagen' 	=> 'placeholder.png',
+    			'visible'	=> true,
+    			'valor'		=> 0,
+    			'next' 		=> 'seleccionarItem(5)',
+    			'sub' 		=> false,
+    		],
+    		'6' => [
+    			'nombre' 	=> 'Campañas',
+    			'imagen' 	=> 'placeholder.png',
+    			'visible'	=> true,
+    			'valor'		=> 0,
+    			'next' 		=> 'seleccionarItem(6)',
+    			'sub' 		=> false,
+    		],
+    	];
+
+    	echo '<pre>';print_r($arr);echo '</pre>';
+    	echo '<hr>';
+    	echo 'Buscar<br>';
+
+    	foreach($arr as $key => $value)
+    	{
+    		
+    	}
+
+    	echo '<hr>';
+    	echo json_encode($arr);
+    	exit;
+        // return $this->render('BaseBundle:Default:index.html.twig');
+    }
+
+    private function buscarArr($arr)
+    {
+
     }
 }
--- src/MobilBundle/Controller/CotizacionController.php
+++ src/MobilBundle/Controller/CotizacionController.php
@@ -25,12 +25,19 @@ class CotizacionController extends Controller
 
     				if($value->item == 0)
     				{
+    					$nombre = 'Sin información';
     					if($producto = $em->getRepository('BaseBundle:ProProducto')->findOneBy(array('proIdPk' => $value->producto )))
 		    			{
-		    				// $datos->nombre = $producto->
+		    				$nombre = $producto->getProNombre();
 		    			}
+		    			$datos->nombre = $nombre;
     				}else{
-    					$datos->tipo = 'item';
+    					$nombre = 'Sin información';
+    					if($producto = $em->getRepository('BaseBundle:ProItem')->findOneBy(array('iteIdPk' => $value->item )))
+		    			{
+		    				$nombre = $producto->getIteNombre();
+		    			}
+		    			$datos->nombre = $nombre;
     				}
     				$datos->producto = $value->producto;
     				$datos->item = $value->item;
@@ -38,8 +45,7 @@ class CotizacionController extends Controller
     				$array[] = $datos;
     			}
 
-    			echo '<pre>';
-    			print_r($array);
+    			echo json_encode(array('result' => $array));
 
     		}else{
     			print_r('Datos');
@@ -50,5 +56,115 @@ class CotizacionController extends Controller
     	}
 
     	exit;
+    }
+
+    public function informacionProductoTotalAction(Request $request)
+    {
+        if( $request->getMethod() == 'POST' )
+        {
+            if($postdata = file_get_contents("php://input"))
+            {
+                $post = json_decode($postdata);
+                $em   = $this->getDoctrine()->getManager('tuciudad_producto');
+
+                $array = array();
+                $total = 0;
+                foreach($post->producto as $value)
+                {
+
+                    if($value->item == 0)
+                    {
+                        if($producto = $em->getRepository('BaseBundle:ProProducto')->findOneBy(array('proIdPk' => $value->producto )))
+                        {
+                            // $nombre = $producto->getProNombre();
+
+                            $array[$value->pedido]['pedido'] = $value->pedido;
+                            $array[$value->pedido]['nombre'] = $producto->getProNombre();
+                            $array[$value->pedido]['foto'] = 'placeholder.png';
+                        }
+                    }else{
+                        if($producto = $em->getRepository('BaseBundle:ProItem')->findOneBy(array('iteIdPk' => $value->item )))
+                        {
+                            $array[$value->pedido]['items'][]['nombre'] = $producto->getIteNombre();
+                            $total += $producto->getIteValor();
+
+                            if(isset($array[$value->pedido]['valor']))
+                            {
+                                $array[$value->pedido]['valor'] += $producto->getIteValor();
+                            }else{
+                                $array[$value->pedido]['valor'] = $producto->getIteValor();
+                            }
+                        }
+                    }
+
+                }
+
+                // print_r($array);
+                echo json_encode(array('result' => $array, 'total' => number_format($total, 0, ',', '.')));
+            }
+        }
+        exit;
+    }
+
+    public function enviarAction(Request $request)
+    {
+        if( $request->getMethod() == 'POST' )
+        {
+            if($postdata = file_get_contents("php://input"))
+            {
+                $post = json_decode($postdata);
+
+                // enviar mail
+                $datos_mail = [
+                    'nombre'    => $post->cotizacion->form->nombre,
+                    'correo'    => $post->cotizacion->form->correo,
+                    'telefono'  => $post->cotizacion->form->telefono,
+                    'mensaje'   => $post->cotizacion->form->observacion,
+                    'to'        => 'jonathanpadilla09@outlook.com',
+                ];
+
+                print_r($datos_mail);
+
+                $this->enviarMail($datos_mail);
+
+                // print_r($productos);
+            }
+        }
+
+        exit;
+    }
+
+    // private function enviarCotizacion($form)
+    // {
+    //     $datos  = array();
+    //     $datos['nombre']    = ($request->get('nombre', false))? $request->get('nombre'): 0;
+    //     $datos['correo']    = ($request->get('correo', false))? $request->get('correo'): 0;
+    //     $datos['telefono']  = ($request->get('telefono', false))? $request->get('telefono'): 0;
+    //     $datos['mensaje']   = stripcslashes(nl2br(htmlentities($request->get('mensaje'))));
+    //     $datos['to']        = 'jonathanpadilla09@outlook.com';
+
+    //     print_r($form);
+    // }
+
+    public function enviarMail($arr = false)
+    {
+        $return = false;
+        if(is_array($arr))
+        {
+            $headers = "From: " . $arr['correo'] . "\r\n";
+            $headers .= "Reply-To: ". $arr['correo']. "\r\n";
+            $headers .= "MIME-Version: 1.0\r\n";
+            $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+            $contenido = $this->renderView('BaseBundle:Default:plantilla_email_ventas.html.twig',array('datos' => $arr));
+            // echo $contenido; exit;
+            if(mail($arr['to'], 'www.tuciudad.cl - '.$arr['nombre'], $contenido, $headers))
+            {
+                $return = true;
+                echo 'enviado';
+            }else{
+                echo 'no enviado';
+            }
+        }
+        return $return;
     }
 }
